64fa4e4050d9eca0c8b1629d6658a002b15ccf9d
unknown <arunne14799@gmail.com>
2025-06-28 20:32:44 +0530

    Initial commit - Spring Boot project

---
+++ ProjectCatalogService/src/test/java/com/backend/ProjectCatalogService/ProjectCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ProjectCatalogService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
